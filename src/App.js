a1cc3dc345bf0b1bf153ca46037ec53c92d7999e
MiriamVidoto <miriamvidoto.hdg@gmail.com>
2023-03-03 16:15:18 -0300

    adiciona ghpages

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div>
       <Route>
         <Switch>
-          <Route exact path="/" component={ Login } />
+          <Route exact path="/project-trybewallet/" component={ Login } />
           <Route path="/carteira" component={ Wallet } />
         </Switch>
       </Route>
