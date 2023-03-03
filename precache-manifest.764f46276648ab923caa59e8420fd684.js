9fd4f7db3c0d4e6c73e884335fedc9b9e6186b15
MiriamVidoto <miriamvidoto.hdg@gmail.com>
2023-03-03 16:12:57 -0300

    Updates

---
+++ precache-manifest.764f46276648ab923caa59e8420fd684.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01fa750da45244bd604213c293f2bf59",
+    "url": "/project-trybewallet/index.html"
+  },
+  {
+    "revision": "b7b1c95c789efd187b80",
+    "url": "/project-trybewallet/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1a84abc2ce34069281df",
+    "url": "/project-trybewallet/static/js/2.450a51d4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/project-trybewallet/static/js/2.450a51d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7b1c95c789efd187b80",
+    "url": "/project-trybewallet/static/js/main.701c6ae2.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/project-trybewallet/static/js/main.701c6ae2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7e9fac6e1746606b827",
+    "url": "/project-trybewallet/static/js/runtime-main.29301b93.js"
+  }
+]);
